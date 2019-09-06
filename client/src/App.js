2387c8d17e4e2645c8becbf8edd05fc62b841484
Raajn Patel <rndpatel@gmail.com>
2019-09-06 08:42:13 -0700

    started writing out app.js. added fetch to obtain the data from localhost, as well as began making a function to display the SoccerPlayers with a state.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            players: []
+        };
+    }
 
-}
-function App() {
-  return (
+    componentDidMount() {
+        fetch('http://localhost:5000/api/players')
+            .then (res => res.json())
+            .then (data => this.setState({players:data}));
+    }
 
-  );
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log(this.state);
+    }
+
+    render(){
+        return(
+            <div className = "App">
+                <SoccerPlayer players = {this.state.players} />
+            </div>
+        );
+    }
 }
 
 export default App;
